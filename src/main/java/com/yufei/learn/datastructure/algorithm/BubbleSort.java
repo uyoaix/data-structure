d6ebee51cd360d3e74fbb9fd54c2481639b389ef
uyoaix <zi_mengling@yeah.net>
2020-04-11 22:37:43 +0800

    交换排序：冒泡排序

--- src/main/java/com/yufei/learn/datastructure/algorithm/BubbleSort.java
+++ src/main/java/com/yufei/learn/datastructure/algorithm/BubbleSort.java
@@ -1,27 +1,22 @@
 package com.yufei.learn.datastructure.algorithm;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.Arrays;
 
 /**
  * 交换排序： 冒泡排序
  */
-@Slf4j
 public class BubbleSort {
 
     public static void main(String[] args) {
 
         int[] array = {2, 3, 17, 10, 32, 12, 4, 16, 20, 1};
 
-        log.info("数组原顺序： " + Arrays.toString(array));
+        System.out.println("排序前顺序： " + Arrays.toString(array));
         sort(array);
-        log.info("数组排序顺序： " + Arrays.toString(array));
-
+        System.out.println("排序后顺序： " + Arrays.toString(array));
     }
 
-    private static int[] sort(int[] array){
-
+    private static void sort(int[] array){
 
         for(int i=0; i<array.length-1; i++){
             for(int j=0; j<array.length-1-i; j++){
@@ -32,10 +27,6 @@ public class BubbleSort {
                 }
             }
         }
-
-        return array;
     }
-
-
 
 }
